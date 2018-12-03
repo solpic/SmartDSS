d683f1e8de9ece8fa73ded66c68094fe7dcb9da2
Merrill Preska Steinberg <meps541@gmail.com>
2018-12-03 16:16:11 -0500

    More reorganization

--- ClientServer/RPCClient.py
+++ ClientServer/RPCClient.py
@@ -1,4 +1,8 @@
 from xmlrpc.client import *
 
-proxy = ServerProxy('http://localhost:9000')
-print(proxy.return_square(10))
+def get_proxy(host):
+	return ServerProxy(host)
+
+# Example
+# proxy = get_proxy('http://localhost:9000')
+# print(proxy.return_square(10)) // prints 100
--- ClientServer/RPCServer.py
+++ ClientServer/RPCServer.py
@@ -3,21 +3,22 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import logging
 
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
-
-server = SimpleXMLRPCServer(('localhost', 9000), logRequests=True)
 
 # Example function
-
 def return_square(num):
 	return num*num
-	
-server.register_function(return_square)
 
-try:
-    print('Use Control-C to exit')
-    server.serve_forever()
-except KeyboardInterrupt:
-    print('Exiting')
-    
+# Set up logging
+def run_server(host, port):
+	logging.basicConfig(level=logging.DEBUG)
+
+	server = SimpleXMLRPCServer((host, port), logRequests=True)
+		
+	server.register_function(return_square)
+
+	try:
+		print('Use Control-C to exit')
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print('Exiting')
+		
