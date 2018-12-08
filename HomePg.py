0ea656de9044229ef74ddd9880278d1e5ac4e058
jacinta-m <jacinta.moore8@gmail.com>
2018-12-07 20:57:52 -0500

    Add files via upload

--- HomePg.py
+++ HomePg.py
@@ -16,6 +16,7 @@ class HomePg():
     def createWidget(self):
         frame1 = Frame(self.parent)
         frame2 = LabelFrame(frame1)
+        frame2.configure(background = "")
         frame2.grid(row=0, column=0, sticky=W)
         frame3 = Frame(frame1)
         frame3.grid(row=1, column=0, sticky=W, padx=120)
@@ -26,9 +27,9 @@ class HomePg():
         l.grid(sticky=W)
 
         Label(frame2, text="", height =0, width=100).grid(row=0, column=1)
-        Label(frame2, text= "SmartDSS", font=('Arial', 48), fg = "dodgerblue").grid(row=0,column=0)
-        Button(frame2, text="Log in", font=('Ariel', 25), command = self.login, height=1, width=10).grid(row=0, column=2, sticky=E, padx=10)
-        Button(frame2, text="Get Started",font=('Arial', 25), command = self.signup, height=1, width=10).grid(row=0, column=3, sticky=E, padx=10)
+        Label(frame2, text= "SmartDSS", font=('Arial', 48), fg = "medium blue").grid(row=0,column=0)
+        Button(frame2, text="Log in", font=('Ariel', 30), fg= "medium blue", borderwidth = 0,  command = self.login, height=1, width=10).grid(row=0, column=2, sticky=E, padx=10)
+        Button(frame2, text="Get Started",font=('Arial', 30), fg= "medium blue", borderwidth = 0, command = self.signup, height=1, width=10).grid(row=0, column=3, sticky=E, padx=10)
 
         frame1.pack()
 
