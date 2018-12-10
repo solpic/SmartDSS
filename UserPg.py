2983b07e8dfb3a7cf7c3eebe59843e381289b3ea
jacinta-m <jacinta.moore8@gmail.com>
2018-12-09 20:01:23 -0500

    Added a lot more widgets, profile pic for UserPg

--- UserPg.py
+++ UserPg.py
@@ -10,18 +10,86 @@ class UserPg():
         parent.geometry("{0}x{1}+0+0".format(
             parent.winfo_screenwidth(), parent.winfo_screenheight()))
         self.parent.configure(background="white")
+        obj1 = Users1.Users()
+        self.rank = obj1.getRank(username)
+        self.Interest1 = obj1.getInterest1(username)
+        self.Interest2 = obj1.getInterest2(username)
+        self.Interest3 = obj1.getInterest3(username)
         self.createWidget()
 
     def createWidget(self):
         frame1 = Frame(self.parent)
         frame1.configure(background = "white")
+        frame2 = Frame(frame1)
+        frame2.config(width = 100)
+        frame2.pack(fill=X, expand=True)
+        frame3 = Frame(frame1)
+        frame3.pack(fill=X, expand=True)
+        frame4 = Frame(frame1)
+        frame4.pack(fill=X, expand=True)
+        frame5 = Frame(frame4)
+        frame5.pack(side = LEFT, fill=X)
+        frame6 = Frame(frame4)
+        frame6.pack(side = LEFT, fill=X)
+        frame7 = Frame(frame4)
+        frame7.pack(side = LEFT, fill=X)
+        frame8 = Frame(frame4)
+        frame8.pack( side = LEFT, fill=X)
+
         UsernameText = self.user
-        ULabel =  Label(frame1,text = UsernameText,font=('Ariel', 30), fg= "medium blue", background ="white" ).grid(row=0, column=0)
+        ULabel =  Label(frame2,text = UsernameText + " " + self.rank, font=('Ariel', 30), fg= "medium blue",
+                        background ="white" ).grid(row=0, column=0, padx =10, pady=20)
+        fillLabel = Label(frame2, text="", width =800).grid(row=0,column=1)
+
+        img = self.getImage()
+        profile = Label(frame5, image= img)
+        profile.image = img
+        profile.grid(row=0, column=0, pady=10)
+
+        InterestLabel = Label(frame5, text= "Interests", font=('Ariel', 30), fg="medium blue", background="white").grid(
+            row=1, column=0, pady=10)
+        Interest1Label = Label(frame5, text= self.Interest1, font=('Ariel', 30), fg="medium blue", background="white")\
+            .grid(row=2, column=0)
+        Interest2Label = Label(frame5, text= self.Interest2, font=('Ariel', 30), fg="medium blue", background="white")\
+            .grid(row=3, column=0)
+        Interest3Label = Label(frame5, text= self.Interest3, font=('Ariel', 30), fg="medium blue", background="white").\
+            grid(row=4, column=0)
+
+        createdocLabel = Button(frame3,text = "create document",font=('Ariel', 30), fg= "medium blue", background ="white" ).grid(row=0, column=0, padx=10, pady=10)
+        doc1Label = Button(frame3, text= "recent doc 1", font=('Ariel', 30), fg="medium blue", background="white", width=15).grid(row=0, column=1, padx=20)
+        docLabel2 = Button(frame3, text= "doc 2", font=('Ariel', 30), fg="medium blue", background="white", width =15).grid(row=0, column=2, padx=10)
+        docLabel3 = Button(frame3, text= " doc 3", font=('Ariel', 30), fg="medium blue", background="white", width =15).grid(row=0, column=3, padx=10)
+
+        searchmember = Label(frame6,text = "    Search members",font=('Ariel', 30), fg= "medium blue", width=15 ).grid(sticky=E)
+        Label(frame6, text="", width=5).grid(row=1, column=0)
+        searchmEntry = Entry(frame6, width=50 ).grid(row=1,column=0, sticky=E, padx=10)
+        simg = PhotoImage(file="images.gif")
+        searchpic = Label(frame6, image= simg)
+        searchpic.image = simg
+        searchpic.grid(row=1, column=1)
+        searchMresult = Listbox(frame6, width=50).grid(row=2,column=0, sticky=E,padx=10)
+
+        searchdoc = Label(frame7, text="Search Documents", font=('Ariel', 28), fg="medium blue", width=15).grid(
+            sticky=E)
+        searchDEntry = Entry(frame7, width=50).grid(row=1, column=0, sticky=E, padx=10)
+        simg = PhotoImage(file="images.gif")
+        searchpicD = Label(frame7, image=simg)
+        searchpicD.image = simg
+        searchpicD.grid(row=1, column=1)
+        searchDresult = Listbox(frame7, width=50).grid(row=2, column=0, sticky=E, padx=10)
+
+
+        compltaboo = Label(frame8,text =  "taboo",font=('Ariel', 30), fg= "medium blue", background ="white" ).grid(row=0, column=0, sticky=E, padx = 50)
+        tabooButton = Button(frame8, text= "Process Complaints", font=('Ariel', 24), fg="medium blue", background="white").grid(row=1, column=0, padx=10)
+
 
         frame1.pack()
 
+    def getImage(self):
+        img = PhotoImage(file="testgirl.gif")
+        return img
 
     def main(self, username):
-        root = Tk()
+        root = Toplevel()
         UserPg(root, username)
         root.mainloop()
--- Users1.py
+++ Users1.py
@@ -5,7 +5,9 @@ class Users():
     def __init__(self):
         self.conn = sqlite3.connect('usertest.db')
         self.c = self.conn.cursor()
-
+        #self.c.execute('''CREATE TABLE t
+        #                    (username text NOT NULL PRIMARY KEY, password text, Fname text, Lname text, Interest1 text,
+        #         Interest2 text, Interest3 text, joindate date, type text)''')
     def createUSer(self, username, password, Fname, Lname, Interest1, Interest2, Interest3, joindate, Application ):
         self.c.execute("INSERT INTO t VALUES (?,?,?,?,?,?,?,?,?)", (username, password, Fname, Lname, Interest1, Interest2,
                                                                Interest3, joindate, Application))
@@ -44,7 +46,33 @@ class Users():
         # the SU sets the application type to OU
 
     # TODO: Requested Features from Arik:
-    def getRank(self):
-        print()
+    def getRank(self, username):
+        user = (username,)
+        self.c.execute('SELECT type FROM t WHERE username= ? ', user)
+        rank = self.c.fetchone()[0]
+
+        print(rank)
+        return rank
     #   Pre: Runs on a user instance
     #   Post: returns the rank of the user instance as a string of "OU" "GU" or "SU"
+
+    def getInterest1(self, username):
+        user = (username,)
+        self.c.execute('SELECT Interest1 FROM t WHERE username= ? ', user)
+        Interest1 = self.c.fetchone()[0]
+        print(Interest1)
+        return Interest1
+
+    def getInterest2(self, username):
+        user = (username,)
+        self.c.execute('SELECT Interest2 FROM t WHERE username= ? ', user)
+        Interest2 = self.c.fetchone()[0]
+        print(Interest2)
+        return Interest2
+
+    def getInterest3(self, username):
+        user = (username,)
+        self.c.execute('SELECT Interest3 FROM t WHERE username= ? ', user)
+        Interest3 = self.c.fetchone()[0]
+        print(Interest3)
+        return Interest3
