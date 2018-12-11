89b95c0cc8604a2299a2bd62fcb1b64b016f5316
Jacinta Moore <jacinta.moore8@gmail.com>
2018-12-11 15:49:31 -0500

    Added doc open

--- UserPg.py
+++ UserPg.py
@@ -1,7 +1,8 @@
 from tkinter import*
-from tkinter import Listbox
-
+from DocumentDB import doc_cli
 import Users1
+import DocumentFileTest
+from DocumentScreenTester import user
 
 class UserPg():
 
@@ -22,6 +23,7 @@ class UserPg():
         self.memberSearch = []
         self.interestsSearch = []
         self.documentSearch = []
+        self.activedoc = ""
         self.invar = StringVar(value=self.interestsSearch)
         self.var = StringVar(value=self.memberSearch)
         self.docvar = StringVar(value= self.documentSearch)
@@ -67,7 +69,7 @@ class UserPg():
         Label(frame5, text= self.Interest3, font=('Ariel', 26), fg="medium blue").\
             grid(row=4, column=0, padx=20, sticky=W)
 
-        Button(frame3,text = "create document",font=('Ariel', 30), fg= "medium blue", background ="white", command = createnewdoc ).grid(row=0, column=0, padx=10, pady=10)
+        Button(frame3,text = "create document",font=('Ariel', 30), fg= "medium blue", background ="white", command = self.createnewdoc ).grid(row=0, column=0, padx=10, pady=10)
         Button(frame3, text= "recent doc 1", font=('Ariel', 30), fg="medium blue", background="white", width=15).grid(row=0, column=1, padx=20)
         Button(frame3, text= "doc 2", font=('Ariel', 30), fg="medium blue", background="white", width =15).grid(row=0, column=2, padx=10)
         Button(frame3, text= " doc 3", font=('Ariel', 30), fg="medium blue", background="white", width =15).grid(row=0, column=3, padx=10)
@@ -82,7 +84,6 @@ class UserPg():
         for entry in self.memberSearch:
             print("entry", entry)
             searchResults.insert(entry)
-
 
         Label(frame6, text="Search Members by Interests", font=('Ariel', 18), fg="medium blue", width=22).grid(row=3, column=0)
         Entry(frame6, textvariable=self.memberInt, width=50).grid(row=4, column=0, sticky=E, padx=10)
@@ -101,11 +102,12 @@ class UserPg():
         searchpicD = Button(frame7, image=simg, command = self.docSearch)
         searchpicD.image = simg
         searchpicD.grid(row=1, column=1)
-        searchResultDocs = Listbox(frame7, listvariable = self.docvar, width=50).grid(row=2, column=0, sticky=E, padx=10)
+        self.searchResultDocs = Listbox(frame7, listvariable = self.docvar, width=50)
+        self.searchResultDocs.grid(row=2, column=0, sticky=E, padx=10)
         for entry in self.documentSearch:
             print("entry", entry)
-            searchResultDocs.insert(entry)
-        Button(frame7,text="Open Document", font=('Ariel', 24), fg="medium blue", width=16, background= "white").grid(row=3, column=0, pady=5)
+
+        Button(frame7,text="Open Document", font=('Ariel', 24), fg="medium blue", width=16, background= "white", command = self.opendoc).grid(row=3, column=0, pady=5)
 
         Button(frame8, text= "Process Complaints", font=('Ariel', 24), fg="medium blue", background="white"
                             ).grid(row=1, column=1, padx=30, pady=40, sticky=E)
@@ -134,14 +136,36 @@ class UserPg():
         self.invar.set(self.interestsSearch)
 
     def docSearch(self):
+        docs = doc_cli.get_all_documents()
         searchItem = self.docName.get()
-        self.documentSearch = self.UserDetailService.searchtestUser(searchItem)
+
+        names = []
+        for doc in docs:
+            if searchItem in doc.docName:
+                entry = (doc.docName, doc.owner, doc.versionNumber)
+                names.append(entry)
+        self.documentSearch = names
         for entry in self.documentSearch:
-            print("bottom entry", entry)
-        self.docvar.set(self.documentSearch)
+            print("search", entry)
+        self.docvar.set(names)
 
     def createnewdoc(self):
         print("new document")
+        doc_name = "New Document Test"
+        versionNo = 0
+        init_contents =""
+        doc_cli.create_document( doc_name, self.user, init_contents)
+        doc_cli.get_document(doc_name, self.user, versionNo)
+        DocumentFileTest.DocumentScreen(user(user), doc_cli.get_document(doc_name, self.user, versionNo))
+
+    def opendoc(self):
+        item = self.searchResultDocs.curselection()
+        idx = item[0]
+        docdetail = self.documentSearch[idx]
+        document = docdetail[0]
+        usern = docdetail[1]
+        versionNo = docdetail[2]
+        DocumentFileTest.DocumentScreen(user(usern), doc_cli.get_document(document, usern, versionNo))
 
 
     def main(self, username):
