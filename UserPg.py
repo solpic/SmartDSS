4882babc7ba87721348db14ec4e1cf96572a2a60
Jacinta Moore <jacinta.moore8@gmail.com>
2018-12-12 09:41:24 -0500

    chnaged buttons

--- UserPg.py
+++ UserPg.py
@@ -7,6 +7,7 @@ import ProcessMember
 import ProcessTabooWord
 import ProcessComplaints
 #import createfilepopup
+import datetime
 
 
 import DocumentFileTest
@@ -85,9 +86,9 @@ class UserPg():
 
         Button(frame3, text="create document", font=('Ariel', 30), fg="medium blue", background="white",
                command=self.createnewdoc).grid(row=0, column=0, padx=10, pady=10)
-        Button(frame3, text=self.userDocs[0][0], command = self.openDoc1, font=('Ariel', 30), fg="medium blue", background="white", width=15).grid( row=0, column=1, padx=20)
-        Button(frame3, text=self.userDocs[1][0], command = self.openDoc2, font=('Ariel', 30), fg="medium blue", background="white", width=15).grid(row=0,column=2, padx=10)
-        Button(frame3, text=self.userDocs[2][0], command= self.openDoc3, font=('Ariel', 30), fg="medium blue", background="white", width=15).grid(row=0, column=3,  padx=10)
+      #  Button(frame3, text=self.userDocs[0][0], command = self.openDoc1, font=('Ariel', 30), fg="medium blue", background="white", width=15).grid( row=0, column=1, padx=20)
+      #  Button(frame3, text=self.userDocs[1][0], command = self.openDoc2, font=('Ariel', 30), fg="medium blue", background="white", width=15).grid(row=0,column=2, padx=10)
+      #  Button(frame3, text=self.userDocs[2][0], command= self.openDoc3, font=('Ariel', 30), fg="medium blue", background="white", width=15).grid(row=0, column=3,  padx=10)
 
         Label(frame6, text="Search Members by Username", font=('Ariel', 16), fg="medium blue", width=24).grid(row=0, column=0)
         Entry(frame6, textvariable=self.memberName, width=50).grid(row=1, column=0, sticky=E, padx=10)
@@ -178,11 +179,12 @@ class UserPg():
         names = []
         for doc in docs:
             if self.user in doc.owner:
-                entry = (doc.docName, doc.owner, doc.versionNumber)
+                entry = (doc.docName, doc.owner, doc.versionNumber, doc.createDate)
                 self.userDocs.append(entry)
+        #sort this array
         for doc in docs:
             if self.user not in doc.owner:
-                entry = (doc.docName, doc.owner, doc.versionNumber)
+                entry = (doc.docName, doc.owner, doc.versionNumber, doc.createDate)
                 self.userDocs.append(entry)
         for entry in self.userDocs:
             print("results", entry)
